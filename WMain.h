e8da1f44b0552407dc6824c10db1f563f6726719
nik11s4@gmail.com <admin>
2020-03-29 01:01:57 +0200

    t1

---
+++ WMain.h
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------------
+
+#ifndef WMainH
+#define WMainH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TMainForm : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *Button1;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TMainForm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TMainForm *MainForm;
+//---------------------------------------------------------------------------
+#endif
